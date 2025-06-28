60a48afa4aeb361516b457e8c72ec182394067fe
It's Adam! <adamfaturrachman701@gmail.com>
2025-06-28 14:49:06 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $host = "localhost"; 
-    $user = "root"; 
-    $password = ""; 
-    $nama_database = "db_adam_d1a240046"; 
-    $db = mysqli_connect($host, $user,$password, $nama_database); 
-?> 
